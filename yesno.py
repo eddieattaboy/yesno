221256b4e484b9be5a8065687ea61014c5396854
Ed <42161575+kramertoast@users.noreply.github.com>
2018-08-23 16:43:17 +0800

    Create yesno.py

---
+++ yesno.py
@@ -0,0 +1,13 @@
+while True :
+	x = raw_input("Y/N\n>")
+  
+	if x.lower() == 'y' :
+		print "awesome"
+		break
+    
+	elif x.lower() == 'n' :
+		print "aw."
+		break
+    
+	else :
+		print "Please enter Y or N"
